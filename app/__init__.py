a792cee41c412ccefba8745682ed90ed9f406c5d
Lord Meme <75008009+MisterMeme95@users.noreply.github.com>
2025-06-23 06:06:43 -0600

    Added my face and the header. (#3)
    Approved

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,22 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/hobbies')
+def hobbies():
+    hobbies = [
+        {
+            "name": "Gaming",
+            "description": "Competitive and casual gaming sessions",
+            "image": "/static/img/gaming.jpg"
+        },
+        {
+            "name": "Anime",
+            "description": "I watch anime and discuss lore with friends",
+            "image": "/static/img/anime.jpg"
+        },
+    ]
+    return render_template("hobbies.html", hobbies=hobbies)
