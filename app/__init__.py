38a2d7e685b380f5566a4fb8bbeabc79c89f1edc
Lord Meme <75008009+MisterMeme95@users.noreply.github.com>
2025-06-23 06:07:57 -0600

    Jinja addition (#4)
    * Added my face and the header.
    
    * Jinja is awesome!

--- app/__init__.py
+++ app/__init__.py
@@ -24,3 +24,18 @@ def hobbies():
         },
     ]
     return render_template("hobbies.html", hobbies=hobbies)
+
+@app.route('/work')
+def work():
+    work_data = [
+        {"role": "Software Intern", "company": "TechCorp", "year": "2024", "description": "Built backend APIs"},
+        {"role": "RA", "company": "Univ Lab", "year": "2023", "description": "Worked on ML models"}
+    ]
+    return render_template("list_section.html", section_title="Work Experience", items=work_data)
+
+@app.route('/education')
+def education():
+    education_data = [
+        {"school": "ABC University", "degree": "CS B.Sc.", "year": "2020–2024", "description": "Focus on AI"},
+    ]
+    return render_template("list_section.html", section_title="Education", items=education_data)
