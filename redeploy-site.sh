a390576f88dc025c6f431002091dd27eef7c9a41
Lord Meme <75008009+MisterMeme95@users.noreply.github.com>
2025-07-13 23:36:36 -0400

    Fixed my script to now be more robust and use background processes.

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,9 +1,11 @@
-tmux kill-server
+#tmux kill-server
+cd /root/flask_repo || exit 1
 git fetch --all
 git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-tmux new -d -s flaskapp
-tmux send-keys -t flaskapp "source ../python3-virtualenv/bin/activate" C-m
-tmux send-keys -t flaskapp "export FLASK_APP=app" C-m
-tmux send-keys -t flaskapp "flask run --host=0.0.0.0 --port=5000" C-m
+systemctl restart myportfolio
+#tmux new -d -s flaskapp
+#tmux send-keys -t flaskapp "source ../python3-virtualenv/bin/activate" C-m
+#tmux send-keys -t flaskapp "export FLASK_APP=app" C-m
+#tmux send-keys -t flaskapp "flask run --host=0.0.0.0 --port=5000" C-m
