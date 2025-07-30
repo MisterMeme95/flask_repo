c4dd85efff4f9c25832fd1f10af42587de1dabc7
root <jonathanamthurin6@gmail.com>
2025-07-30 09:59:11 +0000

    update.

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -2,9 +2,11 @@
 cd /root/flask_repo || exit 1
 git fetch --all
 git reset origin/main --hard
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
+#source python3-virtualenv/bin/activate
+#pip install -r requirements.txt
+#systemctl restart myportfolio
 #tmux new -d -s flaskapp
 #tmux send-keys -t flaskapp "source ../python3-virtualenv/bin/activate" C-m
 #tmux send-keys -t flaskapp "export FLASK_APP=app" C-m
