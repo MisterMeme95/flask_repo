143479d6e9f30bc595024dac15afc9284f30d379
Lord Meme <75008009+MisterMeme95@users.noreply.github.com>
2025-08-10 08:51:00 -0400

    Testing if this change will allow the CI to pass even with no env.

--- app/__init__.py
+++ app/__init__.py
@@ -8,17 +8,23 @@ from playhouse.shortcuts import model_to_dict
 
 app = Flask(__name__)
 
-load_dotenv()
+#load_dotenv()
 
 mydb = MySQLDatabase(None)  # Placeholder
 
-mydb.init(
-    os.getenv("MYSQL_DATABASE"),
-    user=os.getenv("MYSQL_USER"),
-    password=os.getenv("MYSQL_PASSWORD"),
-    host=os.getenv("MYSQL_HOST"),
-    port=3306
-)
+load_dotenv()
+
+if os.getenv("TESTING") == "true":
+    from peewee import SqliteDatabase
+    mydb = SqliteDatabase(':memory:')
+else:
+    mydb = MySQLDatabase(
+        os.getenv("MYSQL_DATABASE"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        host=os.getenv("MYSQL_HOST"),
+        port=3306
+    )
 
 class TimelinePost(Model):
     name = CharField()
